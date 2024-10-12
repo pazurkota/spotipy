085b03debe736e799a2ca7ed3040983d7d2177b6
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-10-12 10:38:48 +0200

    Initialize for Authorization class

---
+++ auth/authorization.py
@@ -0,0 +1,13 @@
+import requests
+from flask import Flask
+from handler.auth_json import AuthJson as auth
+
+class Authorization:
+    def __init__(self, app: Flask):
+        self.app = app
+        self.client_id = auth.get_client_id()
+        self.client_secret = auth.get_client_secret()
+        self.redirect_uri = "http://localhost:8080/callback"
+        self.auth_url = "https://accounts.spotify.com/authorize"
+        self.token_url = "https://accounts.spotify.com/api/token"
+        self.api_base_url = "https://api.spotify.com/v1/"
--- handler/auth_json.py
+++ handler/auth_json.py
@@ -6,3 +6,13 @@ class AuthJson:
         path = os.path.join(userpaths.get_my_documents(), "spotiplay_auth.json")
         with open(path, "r") as f:
             return json.load(f)
+
+    @staticmethod
+    def get_client_id():
+        auth = AuthJson
+        return auth.read_json()['client_id']
+
+    @staticmethod
+    def get_client_secret():
+        auth = AuthJson
+        return auth.read_json()['client_secret']
