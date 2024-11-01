9484342b3eed509446ea77b2ee219e125fb3c5df
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-11-01 18:02:15 +0100

    Added _get_header() method

--- api/player.py
+++ api/player.py
@@ -10,51 +10,23 @@ class SpotifyPlayer:
         self.setup_routes()
 
     def setup_routes(self):
-        @self.app.route('/playlists')
-        def get_playlists():  # test method to check if it works. to be deleted soon
+        def _get_header():
             if 'access_token' not in session:
                 return redirect('/login')
 
             if datetime.now().timestamp() > session['expires_in']:
                 return redirect('/refresh_token')
 
-            headers = {
+            return {
                 'Authorization': f"Bearer {session['access_token']}"
             }
-
-            response = requests.get(self.auth.api_base_url + 'me/playlists', headers=headers)
-            playlists = response.json()
-
-            return jsonify(playlists)
 
         @self.app.route('/play')
         def play_playback():
-            if 'access_token' not in session:
-                return redirect('/login')
-
-            if datetime.now().timestamp() > session['expires_in']:
-                return redirect('/refresh_token')
-
-            headers = {
-                'Authorization': f"Bearer {session['access_token']}"
-            }
-
-            requests.put(self.auth.api_base_url + 'me/player/play', headers=headers)
-
+            requests.put(self.auth.api_base_url + 'me/player/play', headers=_get_header())
             return 'Successfully played music!'
 
         @self.app.route('/stop')
         def stop_playback():
-            if 'access_token' not in session:
-                return redirect('/login')
-
-            if datetime.now().timestamp() > session['expires_in']:
-                return redirect('/refresh_token')
-
-            headers = {
-                'Authorization': f"Bearer {session['access_token']}"
-            }
-
-            requests.put(self.auth.api_base_url + 'me/player/pause', headers=headers)
-
+            requests.put(self.auth.api_base_url + 'me/player/pause', headers=_get_header())
             return 'Successfully stopped music!'
