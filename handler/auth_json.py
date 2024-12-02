4e49a1d12ac4f209c92cff9db4c2dd631ae51c25
Piotr Jelonek <piotrek.jelonek@icloud.com>
2024-12-02 18:48:58 +0100

    Added "save_auth_tokens" and "read_auth_tokens"

--- handler/auth_json.py
+++ handler/auth_json.py
@@ -16,3 +16,19 @@ class AuthJson:
     def get_client_secret():
         auth = AuthJson
         return auth.read_json()['client_secret']
+
+    @staticmethod
+    def save_auth_tokens(access_token: str, refresh_token: str, expires_in: int):
+        tokens = {
+            'access_token': access_token,
+            'refresh_token': refresh_token,
+            'expires_in': expires_in
+        }
+
+        with open(os.path.join(userpaths.get_my_documents(), "spotiplay_auth_tokens.json"), "w") as f:
+            json.dump(tokens, f)
+
+    @staticmethod
+    def read_auth_tokens():
+        with open(os.path.join(userpaths.get_my_documents(), "spotiplay_auth_tokens.json"), "r") as f:
+            return json.load(f)
